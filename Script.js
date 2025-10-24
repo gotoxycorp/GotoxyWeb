311bf98ec2cb8d0dfc70a85c730613ac4a472132
Lucila Romeo <lucila.romeo03@gmail.com>
2025-10-24 15:19:29 -0300

    Update Web

--- Script.js
+++ Script.js
@@ -4,17 +4,28 @@ document.getElementById("homeBtn")?.addEventListener("click", () => {
 });
 
 // --- ABOUT US CAROUSEL ---
-const checkLink = document.getElementById("checkLink");
-const carousel = document.getElementById("gameCarousel");
+// Seleccionamos el carrusel y el link
+const gameCarousel = document.getElementById('gameCarousel');
+const checkLink = document.getElementById('checkLink');
 
-// Cada slide tiene su link en data-link
-carousel.addEventListener("slid.bs.carousel", function () {
-  const activeItem = carousel.querySelector(".carousel-item.active");
-  const link = activeItem.getAttribute("data-link");
-  checkLink.href = link;
+// Cada vez que el carrusel cambie de slide
+gameCarousel.addEventListener('slid.bs.carousel', function () {
+    // Buscamos el item activo
+    const activeItem = gameCarousel.querySelector('.carousel-item.active');
+    // Tomamos el data-link
+    const newLink = activeItem.getAttribute('data-link');
+    // Actualizamos el href del link
+    checkLink.href = newLink;
 });
+
 
 // --- GOTXY TEST BUTTON ---
 document.getElementById("gotoxyTest")?.addEventListener("click", () => {
   window.location.href = "index2.html";
 });
+    document.querySelectorAll('#gameCarousel .carousel-item').forEach(item => {
+            item.addEventListener('click', () => {
+                const link = item.getAttribute('data-link');
+                if (link) window.open(link, "_blank");
+            });
+        });
